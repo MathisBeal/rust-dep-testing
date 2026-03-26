a04f97b7a8f75d35f20f56898b4282c35fb0e3d6
Mathis BEAL <mathis.beal04@gmail.com>
2026-03-26 07:49:55 +0100

    update security workflow

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,17 @@ fn main() {
     let now = chrono::Local::now();
 
     println!("Current time: {}", now);
+
+    let time = time::now_utc();
+
+    println!("Current UTC time: {:?}", time);
+
+    let json_str = r#"{"key": "value"}"#;
+
+    let json_value: serde_json::Value = serde_json::from_str(json_str).unwrap();
+
+    println!("Parsed JSON: {}", json_value);
+
+    let img = image::open("example.png").unwrap();
+    println!("Opened image: {:?}", img.as_bytes());
 }
